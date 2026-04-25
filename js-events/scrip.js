140b4a3111c404513badfc78491d1f66ca1e971c
Hamster <hamnoemireyeshrdz@gmail.com>
2026-04-25 19:40:59 +0000

    class 19.1

--- js-events/scrip.js
+++ js-events/scrip.js
@@ -15,6 +15,13 @@
 
 function Articleone(){
     let one = "We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.";
-    alert("Article of faith 1: "  + one);
-
+    alert("The Article one is: " + one);
+}
+function Articletwo(){
+    let two = "We believe that men will be punished for their  own sins, and not for Adam's transgression.";
+    alert("The Article two is: " +two);
+}
+function Articlethree(){
+   let three = "We believe that through the Atonement of Christ, all mankind may be save, by obedience to the laws and oridnances of the Gospel.";
+    alert("The Article three is: " +three);
 }
