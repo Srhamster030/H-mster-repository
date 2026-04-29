e3edfb2627bc94c20bc6098e93428b79e2cd5f02
Hamster <hamnoemireyeshrdz@gmail.com>
2026-04-29 20:21:47 +0000

    commit

---
+++ js-coin/script.js
@@ -0,0 +1,13 @@
+functioin coin(){
+    let randomNum = Math.random();
+    console.long(randomNum);
+    let result =
+    let subtitle = document.querySelector("h2");
+
+    if (randomNum < 0.5){
+        subtitle.innerHTML = "Heads"
+        else{
+            subtitle.innerHTML = "Tails";
+        }
+    }
+    }
