539593999ac355d66279478fb46d544a9211926f
Hamster <hamnoemireyeshrdz@gmail.com>
2026-05-13 20:22:06 +0000

    LAST COMMIT

--- Intervals/script.js
+++ Intervals/script.js
@@ -1,4 +1,7 @@
-let counter = 3;
+// document.addEventListener("DOMContentLoaded", function(){
+//     const dp.wav = new Audio("alarm/dp.wav");
+
+let counter = 10;
 
 function count() {
     counter--;
@@ -7,10 +10,30 @@ function count() {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function(){
-    setInterval(count, 1000);
-    setTimeout(count, 0);
-    document.querySelector("button").disabled = true;
+    setInterval(count, 100);
     };
 });
+let counter = 12;
 
-if count 
+function count() {
+    counter--;
+    document.querySelector('h2').innerHTML = counter;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = function(){
+    setInterval(count, 100);
+    };
+});
+// window.addEventListener("button", function(event){
+//         if(event.key == "a") {
+//             dp.currentTime = 9;
+//             dp.play();
+//             dpDiv.classList.add("active");
+//         }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('button').onclick = function(){
+//     setInterval(count, 100);
+    };
+});
